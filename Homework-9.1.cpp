629787b565ede98f4c7e51418655be0cef62fec6
SkeTch <sketch13rus@mail.ru>
2023-12-15 15:41:32 +0300

    Initial commit

---
+++ Homework-9.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+template <class T>
+void move_vectors(std::vector <T>& lhs, std::vector <T>& rhs) {
+    rhs = std::move(lhs);
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one, two);
+
+    for (const auto elem : two) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
